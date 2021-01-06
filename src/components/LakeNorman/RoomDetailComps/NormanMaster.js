2d95749dd1832ce7085694a209fc0a8a089e2c96
jhf1203 <jhf1203@gmail.com>
2021-01-05 21:20:23 -0500

    Updated Master

--- src/components/LakeNorman/RoomDetailComps/NormanMaster.js
+++ src/components/LakeNorman/RoomDetailComps/NormanMaster.js
@@ -24,10 +24,10 @@ function NormanMaster () {
             <div className="row selection-row">
                 <div className="col-md-9 selection-col">
                 <div className="row">
-                        <h6 className="selection-title">Oak Brûlé Waterfall Nightstands & Dresser</h6>
+                        <h6 className="selection-title">Oak Brûlé Waterfall Nightstands, Dresser, & Shelving</h6>
                     </div>
                     <div className="row">
-                        <p className="info-text">Brûlé Umber | 36"W x 20"D x 22"H (Nightstands), 101"W x 20"D x 28.5"H (Dresser)
+                        <p className="info-text">Brûlé Umber | 36"W x 20"D x 22"H (Nightstands), 101"W x 20"D x 28.5"H (Dresser), 84"W x 14"D x 23"H (Shelving)
                         </p>
                     </div>
                     <div className="row">
@@ -37,19 +37,112 @@ function NormanMaster () {
                         <p className="selection-desc">
                         The natural wood is protected by a clear sealant after it's finished with high-heat.  This heat creates purposeful imperfections; but the wood itself is ultra-stable and will not warm, bend, or crack further with use.
                         </p>
+                        <p className="selection-desc">
+                        We match more collections in this room than in others, since it's the owner's private suite, is used daily, and is meant to be more minimal in nature with the focal point of the room being the lake view from the large windows.  With the nightstands placed on either side of the bed, the dresser will house a large TV mounted above.  The open shelving will be repeated along the wall facing the water to provide additional storage in the space for personal items/artifacts.
+                        </p>
                     </div>          
                 </div>
                 <div className="col-md-3 selection-col mobile-divider">
                     <div className="row img-dual"></div>
                     <div className="row double-display">
-                        <img className="selection-img img-dual" src="https://media.restorationhardware.com/is/image/rhis/prod17450652_E81188298_F_Frank?$PD$&illum=0&wid=650" alt="Brule Dresser" height="50%" width="100%" />
+                        <img className="selection-img img-dual" src="https://media.restorationhardware.com/is/image/rhis/prod17450652_E81188298_F_Frank?$PD$&illum=0&wid=650" alt="Brule Dresser" height="30%" width="100%" />
                     </div>
                     <div className="row double-display">
-                        <img className="selection-img" src="https://media.restorationhardware.com/is/image/rhis/prod18080038_E212453795_F?$PD$&illum=0&wid=650" alt="Brule Nightstand" height="50%" width="100%" />
+                        <img className="selection-img img-dual" src="https://media.restorationhardware.com/is/image/rhis/prod18080038_E212453795_F?$PD$&illum=0&wid=650" alt="Brule Nightstand" height="30%" width="100%" />
+                    </div>
+                    <div className="row double-display">
+                        <img className="selection-img img-dual" src="https://media.restorationhardware.com/is/image/rhis/prod17340171_E511779990_F?$PD$&illum=0&wid=650" alt="Brule Console" height="30%" width="100%" />
                     </div>
                 </div>
             </div>
-            
+            <div className="row selection-row ">
+                <div className="col-md-5 selection-col">
+                    <img className="selection-img" src="https://cdn.shopify.com/s/files/1/2718/0696/products/BIANBIA-03IVML_25_1200x.jpg?v=1608772959" alt="Rug" height="100%" width="100%" />
+                </div>
+                <div className="col-md-7 selection-col mobile-divider">
+                    <div className="row">
+                        <h6 className="selection-title">Bianca Rug</h6>
+                    </div>
+                    <div className="row">
+                        <p className="info-text">11'6" x 15'</p>
+                    </div>
+                    <div className="row">
+                        <p className="selection-desc">
+                        Power-loomed of polypropylene and polyester pile in Turkey, the Bianca Collection is constructed to withstand heavy foot traffic. From contemporary designs to traditional patterns, this highly durable collection offers an option for all design styles.
+                        </p>
+                        <p className="selection-desc">
+                        This rug was selected in part because it encapsulates almost the entire color palette of the home.  In contrast to the neutral, matching funiture selections to inspire minimalism this rug exists within this context to celebrate the beauty of the entire home itself each time the owner uses the space.
+                        </p>
+                    </div>
+                </div> 
+            </div>
+            <div className="row selection-row">
+                <div className="col-md-7 selection-col">
+                <div className="row">
+                        <h6 className="selection-title">Jens Bench | Jens Risom</h6>
+                    </div>
+                    <div className="row">
+                        <p className="info-text">65.5"W x 18"D x 20"H | Ducale Wool | Robin's Egg
+                        </p>
+                    </div>
+                    <div className="row">
+                        <p className="selection-desc">
+                        Jens Risom believed good design automatically made for versatility. “Good design means that anything good will go well with other equally good things,” he was fond of saying.
+                        </p>
+                        <p className="selection-desc">
+                        The Jens Bench (2008), designed by Jens Risom, is an extension of the designer’s Jens Chair and Ottoman, designed in 1949 for the Caribe Hilton Hotel in Puerto Rico but eventually taken out of production. Risom worked closely with us to relaunch the chair and ottoman and develop the bench, from reviewing prototypes to suggesting cost savings. Like all his furniture, the Jens Collection has a subtle Scandinavian sensibility that works in modern or traditional settings. Made in U.S.A.
+                        </p>
+                        
+                    </div>          
+                </div>
+                <div className="col-md-5 selection-col mobile-divider">
+                        <img className="selection-img dwr-img" src="https://images.hermanmiller.group/m/43801b1a26408664/W-PD_1925_FRAME_walnut_MATERIAL_fabric_UPHOLSTERY_ducale_wool_COLOR_robins_egg-jpg.png?blend-mode=darken&blend=f8f8f8&trim-color=ffffff&trim=color&bg=f8f8f8&auto=format&w=1200&q=68&h=1000" alt="Jens Bench" height="40%" width="100%" />
+                    
+                </div>
+            </div>
+            <div className="row selection-row ">
+                <div className="col-md-7 selection-col">
+                    <img className="selection-img" src="https://static.wixstatic.com/media/6a62aa_44ce598aa9ad48d0bdd080d7847d5656~mv2.jpg/v1/fill/w_383,h_420,al_c,q_85,usm_0.66_1.00_0.01/6a62aa_44ce598aa9ad48d0bdd080d7847d5656~mv2.webp" alt="art" height="100%" width="100%" />
+                </div>
+                <div className="col-md-5 selection-col mobile-divider">
+                    <div className="row">
+                        <h6 className="selection-title">"You, You Will Be Queen" | Bas van den Hurk</h6>
+                    </div>
+                    <div className="row">
+                    <p className="info-text">2017 | Silkscreen, oil paint, and pigment on silk | 51"W x 67"H</p>
+                    </div>
+                    <div className="row">
+                        <p className="selection-desc">
+                        Artist Bas Van Den Hurk’s silkscreens are covered in washes of color, their markings as delicate as shadows. Hazy and diffuse, they rarely have a fixed center. Instead, the compositions appear as allover fields of subtly expressed forms and tones. His work is process based, a reflection of the actions and collaborations that went into its making. 
+                        </p>
+                        <p className="selection-desc">
+                        Van Den Hurk’s artistic practice is a continually evolving intellectual inquiry, one that is perpetually unresolved. As a reflection of this, he considers all of his works to be technically unfinished; he believes that all artworks are in a permanent state of instability and development. A frequent collaborator with other artists, he says, “I’m in a permanent dialogue; my works can develop and appear and reappear in new contexts. I can discuss them in new ways; I can paint over them or reuse them.” 
+                        </p>
+                        
+                    </div>
+                </div> 
+            </div>
+            <div className="row selection-row">
+                <div className="col-md-6 selection-col">
+                <div className="row">
+                        <h6 className="selection-title">Womb Chair & Ottoman | Eero Saarinen</h6>
+                    </div>
+                    <div className="row">
+                        <p className="info-text">40"W x 34"D x 35.5"H (chair), 25.5"W x 20"D x 16"H (ottoman) | Cato | Green
+                        </p>
+                    </div>
+                    <div className="row">
+                        <p className="selection-desc">
+                        When Florence Knoll challenged Eero Saarinen to create a chair that she could curl up in, she found the right candidate for the task. The Womb Chair and Ottoman (1946) feature enveloping forms that continue as one of the most iconic representations of midcentury organic modernism. Applying foam molded over a fiberglass shell created a single-piece form that perfectly facilitates a relaxed posture.
+                        </p>
+                        
+                    </div>          
+                </div>
+                <div className="col-md-6 selection-col mobile-divider">
+                        <img className="selection-img dwr-img" src="https://images.hermanmiller.group/m/77bfffbba1ece9be/W-DWR_9097_100145347_green_black_a.png?blend-mode=darken&blend=f8f8f8&trim-color=ffffff&trim=color&bg=f8f8f8&auto=format&w=2000&h=1000&q=60" alt="Womb" height="100%" width="100%" />
+                    
+                </div>
+            </div>
             <div className="row selection-bottom-row"></div>
         </div>
         
